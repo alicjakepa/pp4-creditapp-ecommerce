2d411016afb817a83b8fa58c9bfc441978a54fa9
alicjakepa <s218489@student.uek.krakow.pl>
2022-06-18 18:32:40 +0200

    Describe specifications for sales

---
+++ src/main/java/pl/akepa/sales/Sales.java
@@ -0,0 +1,11 @@
+package pl.akepa.sales;
+
+public class Sales {
+    public Offer getCurrentOffer(String customerId) {
+        return null;
+    }
+
+    public void addToCart(String customerId, String productId) {
+
+    }
+}
